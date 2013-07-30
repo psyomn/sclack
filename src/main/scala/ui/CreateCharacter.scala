e0407676a8703b56ce59201167ca90677c3a497f
psyomn <lethaljellybean@gmail.com>
2013-07-30 01:50:10 -0400

    Minor aesthetic additions to the swing window.
    ... because that would make a difference and make swing look any better anyway.

--- src/main/scala/ui/CreateCharacter.scala
+++ src/main/scala/ui/CreateCharacter.scala
@@ -1,10 +1,14 @@
 package sclack.ui
 
 import javax.swing.ImageIcon
+import javax.swing.border.EmptyBorder
 import java.awt.Dimension
 import swing._
 import swing.event._
 import scala.swing._
+
+/* lib */
+import sclack.domain.{Character, Rogue, Fighter, Wizard}
 
 /**
  * Dialog where we can create a particular character for playing the game.
@@ -21,9 +25,25 @@ class CreateCharacter extends Dialog {
   var cancel       : Button             = new Button{ text = cancelText }
   var characterIco : Label              = new Label{ text = "todo char ico" }
 
+  /* Stats */
+  var player       : Character          = new Character
+
+  var constitutionLabel : Label = 
+    new Label{ text = "Constitution : " + player.constitution }
+  var intelligenceLabel : Label = 
+    new Label{ text = "Intelligence : " + player.intelligence }
+  var strengthLabel     : Label = 
+    new Label{ text = "Strength : " + player.strength }
+
+  var statsList         : Array[Label] = Array[Label](constitutionLabel, intelligenceLabel, strengthLabel)
+
   /* UI Setup start */
   title   = "Create your character" 
   modal   = true
+
+  preferredSize = new Dimension(400,400)
+  maximumSize   = new Dimension(400,400)
+  minimumSize   = new Dimension(400,400)
 
   /* Make the radios */
 
@@ -31,19 +51,30 @@ class CreateCharacter extends Dialog {
     contents += ok
     contents += cancel
   }
- 
+
   val classRadios = new BoxPanel(Orientation.Horizontal) {
     for (rc <- radioClasses) radios :+= new RadioButton(rc)
-    radioGroup = new ButtonGroup(new RadioButton{text= "alksdj"})
+    radioGroup = new ButtonGroup(radios:_*)
     contents ++= radios
   }
 
-  contents = new BorderPanel{
-    import BorderPanel.Position._
-    layout(classRadios)   = North
-    layout(characterIco)  = Center
-    layout(bottomButtons) = South
+  val buttonsAndRadios = new BoxPanel(Orientation.Vertical) {
+    contents += classRadios
+    contents += bottomButtons
   }
+
+  val statsLabels = new BoxPanel(Orientation.Vertical){
+    contents ++= statsList
+  }
+
+  contents = new BorderPanel {
+    import BorderPanel.Position._
+    layout(statsLabels)      = East
+    layout(characterIco)     = Center
+    layout(buttonsAndRadios) = South
+    border = new EmptyBorder(10,10,10,10)
+  }
+
   /* UI Setup end */
 
   centerOnScreen()
